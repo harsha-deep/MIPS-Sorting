0420487b5b4a5a3016d3716405bcc4468fa8dcd7
Harsha <77527524+harsha-deep@users.noreply.github.com>
2021-12-05 00:54:10 +0530

    Update bubblesort.cpp

--- src/bubblesort.cpp
+++ src/bubblesort.cpp
@@ -2,21 +2,22 @@
 #include <algorithm>
 int main()
 {
-	int n;
-	std::cout << "Enter the length of the list:" << std::endl;
-	std::cin >> n;
-	int arr[n];
-	for (int i = 0; i < n; ++i)
-		std::cin >> arr[i];
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n - i - 1; j++)
-		{
-			if (arr[j] > arr[j + 1])
-				std::swap(arr[j], arr[j + 1]);
-		}
-	}
-	for (int i = 0; i < n; ++i)
-		std::cout << arr[i] << " ";
-	return 0;
+    int n;
+    std::cout << "Enter the length of the list: ";
+    std::cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i)
+        std::cin >> arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+                std::swap(arr[j], arr[j + 1]);
+        }
+    }
+    for (int i = 0; i < n; ++i)
+        std::cout << arr[i] << " ";
+    std::cout << "\n";
+    return 0;
 }
